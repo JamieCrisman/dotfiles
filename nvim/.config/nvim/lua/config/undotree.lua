bc1b108aa985885b7a4f1e727152a2b71be313bf
Jamie Crisman <james@longest.voyage>
2022-11-02 20:55:07 +0900

    CR

--- nvim/.config/nvim/lua/config/undotree.lua
+++ nvim/.config/nvim/lua/config/undotree.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.setup()
-  vim.api.nvim_set_keymap("n", "<leader>u", ":UndotreeToggle", { noremap = true, silent = true })
+  vim.api.nvim_set_keymap("n", "<leader>u", ":UndotreeToggle<CR>", { noremap = true, silent = true })
 end
 
 return M
